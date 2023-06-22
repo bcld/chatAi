c122bfcb864a809421e8def811b7327335606c23
Juderek <5389554+Juderek@user.noreply.gitee.com>
2023-06-22 14:18:54 +0800

    空格

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -380,7 +380,7 @@ export const ChatInput = ({
         </div>
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
-        Base on  
+        Base on &nbsp;
         <a
           href="https://github.com/mckaywrigley/chatbot-ui"
           target="_blank"
